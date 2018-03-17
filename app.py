3f496c58f229d81c8b87faffb47ea196bd0b664d
vitoloper <vitoloper@users.noreply.github.com>
2018-03-17 16:04:01 +0100

    Add image upload

--- app.py
+++ app.py
@@ -1,18 +1,44 @@
 import os
-import random
 import time
+from werkzeug.utils import secure_filename
 from flask import Flask, request, render_template, session, flash, redirect, \
-    url_for, jsonify
+    url_for, jsonify, send_from_directory
 from celery import Celery
+
+# InvalidUsage exception
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
 
 # Initialize Flask
 flask_app = Flask(__name__)
 
-# Celery configuration
+
+# Configuration
 flask_app.config.update(
     CELERY_BROKER_URL='amqp://',
-    CELERY_RESULT_BACKEND='amqp://'
+    CELERY_RESULT_BACKEND='amqp://',
+    UPLOAD_FOLDER='upload'
 )
+
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 def make_celery(app):
     celery = Celery(
@@ -36,17 +62,43 @@ def make_celery(app):
 # Create Celery object
 celery = make_celery(flask_app)
 
+
 # Import tasks (after the celery object is created)
 from tasks import predict as tasks_predict
+
 
 # Flask routes
 
 @flask_app.route('/predict', methods=['POST'])
 def predict():
-
-    # TODO: upload image, call Celery predict task
-    tasks_predict.delay(2, 3)
+    # check if the POST request has the file part
+    print(request.files)
+    if 'file' not in request.files:
+        return jsonify({'error': 'no file key'}), 400
+        # raise InvalidUsage('No file part', status_code=400)
+    
+    file = request.files['file']
+    
+    # browser also submit an empty part without filename if user does not select a file
+    if file.filename == '':
+        return jsonify({'error': 'empty filename'}), 400
+        # raise InvalidUsage('No selected file', status_code=400)
+    
+    # check if file extension is allowed
+    if not file or not allowed_file(file.filename):
+        return jsonify({'error': 'extension not allowed'}), 400
+        # raise InvalidUsage('File extension not allowed', status_code=400)
+    
+    # save file
+    filename = secure_filename(file.filename)
+    f_name, f_ext = filename.rsplit('.', 1)
+    timestamp = str(int(time.time()))   # seconds since Epoch
+    file.save(os.path.join(flask_app.config['UPLOAD_FOLDER'], f_name + '-' + timestamp + '.' + f_ext))
+    
+    # TODO: long running computation intensive task here
+    
     return jsonify({'status': 'GOT IT'}), 202
+
 
 @flask_app.route('/status/<task_id>')
 def status(task_id):
