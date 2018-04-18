a6ac1de241df1be6c3a2ae984096e3026421717c
vitoloper <vitoloper@users.noreply.github.com>
2018-04-18 14:40:38 +0200

    start-flask.sh: create upload dir if it does not exist

--- start-flask.sh
+++ start-flask.sh
@@ -1,3 +1,9 @@
+# Create upload directory if it does not exist
+UPLOAD_FOLDER="upload"
+if [ ! -d "$UPLOAD_FOLDER" ]; then
+  mkdir "$UPLOAD_FOLDER"
+fi
+
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
 flask run
