2426d098ce022876d4225c4e7127ccab3f56e487
vitoloper <vitoloper@users.noreply.github.com>
2018-04-22 17:45:20 +0200

    start-flask.sh bind to 0.0.0.0 option

--- start-flask.sh
+++ start-flask.sh
@@ -6,4 +6,7 @@ fi
 
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
+
+# You may want to bind to host 0.0.0.0 (e.g. if you build a Docker image or need full access)
 flask run
+# flask run --host=0.0.0.0
