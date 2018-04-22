20eca1843b360cd8571be6282d8f9dfd9204642f
vitoloper <vitoloper@users.noreply.github.com>
2018-04-22 17:39:57 +0200

    use ConvnetBuilder and load_state_dict

--- tasks.py
+++ tasks.py
@@ -17,22 +17,25 @@ sz=224
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 # Load the model
-model = torch.load(celery.conf['torch_model'], map_location=lambda storage, location: storage)
+# model = torch.load(celery.conf['torch_model'], map_location=lambda storage, location: storage)
+
+convnet = ConvnetBuilder(resnet34, c=2, is_multi=False, is_reg=False, xtra_cut=0, custom_head=None)
+convnet.model.load_state_dict(torch.load(celery.conf['torch_model'], map_location=lambda storage, location: storage))
 
 @celery.task(bind=True)
 def predict(self, filename):
     self.update_state(state='PROGRESS', meta={'status':'Predicting'})
     
     # We don't care about trn_tfms, hence the _
-    _, val_tfms = tfms_from_model(model, sz)
+    _, val_tfms = tfms_from_model(convnet.model, sz)
     im = val_tfms(open_image(filename))
     
     # Use the CPU for prediction
     # T(x): make a tensor
     # V(x): make a Variable (A PyTorch Variable is a wrapper around a PyTorch Tensor, and represents a node in a computational graph.)
     # http://pytorch.org/tutorials/beginner/pytorch_with_examples.html#pytorch-variables-and-autograd
-    model.eval()
-    preds = to_np(model(V(T(im[None]))))
+    convnet.model.eval()
+    preds = to_np(convnet.model(V(T(im[None]))))
     np.exp(preds)
 
     print(np.argmax(preds))     # 0: cat, 1: dog 
